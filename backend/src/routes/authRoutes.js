67dbf4983ab9c3588e84ea166170c90b0ea21e44
Kunal Rawal <114874006+kunalrawal78@users.noreply.github.com>
2025-09-27 19:42:03 +0530

    authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -5,5 +5,13 @@ const router = express.Router();
 
 
 router.post("/login", login);
+router.get("/", (req, res) => {
+  res.json({ message: "Auth API is working!" });
+});
+
+// In notesRoutes.js
+router.get("/test", (req, res) => {
+  res.json({ message: "Notes API is running" });
+});
 
 export default router;
