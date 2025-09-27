a0f804347c75c16550c05aa53fdbf9db2ca474e5
Kunal Rawal <114874006+kunalrawal78@users.noreply.github.com>
2025-09-28 00:17:26 +0530

    axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API_BASE_URL = 'https://penguin-apps-task.vercel.app';
 const axiosInstance = axios.create({
-    baseURL: "https://penguin-apps-task-zn58.vercel.app/api/auth"
+    baseURL: "https://penguin-apps-task-zn58.vercel.app"
 });
 
 axiosInstance.interceptors.request.use((config)=>{
